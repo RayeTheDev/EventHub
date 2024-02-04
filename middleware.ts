72eee3a48d43206944212ac9f0de2f9083ac1411
Turbold <turbolddev@gmail.com>
2024-02-04 16:43:46 +0800

    deploy update

--- middleware.ts
+++ middleware.ts
@@ -6,10 +6,10 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
-    "events/:id",
-    "api/webhook/clerk",
-    "api/webhook/stripe",
-    "api/uploadthing",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
   ],
   ignoredRoutes: ["api/webhook/clerk", "api/webhook/stripe", "api/uploadthing"],
 });
